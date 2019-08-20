4558613560d8ee75519989a552a477916e299aa6
TabishAjaz <tabish.ajaz786@gmail.com>
2019-08-20 10:58:34 +0530

    complete code of infosys airline ticket printing python question according to airlines source destination no.of passengers

--- Airlines_ticket_printing.py
+++ Airlines_ticket_printing.py
@@ -1,1 +1,16 @@
-print("hello")
+def generate_ticket(airline,source,destination,no_of_passengers):
+    ticket_number_list=[]
+    if(no_of_passengers>=5):
+        for i in range(no_of_passengers-5,no_of_passengers):
+            a=str(i+101)
+            ticket_number_list.append(airline+":"+source[:3]+":"+destination[:3]+":"+a)
+    #Write your logic here
+    else:
+        for i in range(no_of_passengers):
+            a=str(i+101)
+            ticket_number_list.append(airline+":"+source[:3]+":"+destination[:3]+":"+a)
+    #Use the below return statement wherever applicable
+    return ticket_number_list
+
+#Provide different values for airline,source,destination,no_of_passengers and test your program
+print(generate_ticket("AI","Sydney","London",7))
